8ee68fb43e8418af5c25ade8d95febb0ecb42995
Avinash3003 <avinashreddy5142@gmail.com>
2025-07-21 10:41:03 +0530

    Updated Summary content

--- ChildComponents/SummaryContent.jsx
+++ ChildComponents/SummaryContent.jsx
@@ -31,7 +31,7 @@ function SummaryContent() {
     <div className='p-4 sm:p-6 pb-12 text-gray-800'>
       <div className='bg-white shadow-md rounded-xl p-6'>
         <h1 className='text-3xl font-serif sm:text-4xl font-semibold mb-4 text-blue-800'>Hi, I'm Avinash!</h1>
-        <p className='text-base sm:text-lg leading-relaxed text-justify'>
+        <p className='text-base sm:text-lg leading-relaxed text-left break-words hyphens-auto sm:break-normal sm:hyphens-none'>
           I'm a 2025 graduate with a strong passion for technology and innovation. I take great satisfaction in my dedication to my work and ongoing exploration of my interests, particularly in the domains of data science, software development, and artificial intelligence.
           I like using clever, scalable solutions to solve real-world issues, and I'm constantly willing to pick up new skills, adjust, and make a significant contribution to any team or project I work on.
         </p>
